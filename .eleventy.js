5ef8596db24c0549e10e79d9c967fcd0ea8cc34b
yhorian <liam.jm.martin@gmail.com>
2022-12-08 22:41:49 +0000

    testing lazy loading on all markdown images

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
-const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const htmlmin = require("html-minifier");
 const PostCSSPlugin = require("eleventy-plugin-postcss");
+const lazy_loading = require('markdown-it-image-lazy-loading');
+
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -18,12 +18,10 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.amendLibrary("md", mdLib => mdLib.use(lazy_loading, { base_path: "./src" ,image_size: true,decoding: true}));
+
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
-
-  // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
